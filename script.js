83eed8c8e4f79bd1e3b1643fbff278c7e30269dc
kiritosate <77735293+kiritosate@users.noreply.github.com>
2021-11-02 20:08:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+
+
+
+function Members(){
+    var members = [
+    'kiritosate',
+    'maricar prii',
+    'katrina keithHHH',
+    'insect',
+    'bussot',
+    'uncle bussot',
+    'alt+f4',
+    'gurilla',
+    'salsalani',
+    'carlo belleza',
+    'angelita cute',
+]
+
+var memberLen = members.length;
+
+for(var i=0; i<memberLen; i++){
+
+    var context = members[i];
+
+    var listObj = document.createElement('li');
+    listObj.className = "list-group-item name-list";
+    var node = document.createTextNode(context);
+    listObj.appendChild(node);
+
+    document.querySelector(".list-group").appendChild(listObj);
+
+}
+}
+
+
+for(var i=50; i<100;i++){
+
+    var itemList = document.createElement("li");
+    itemList.className = "list-group-item ";
+    var imageElem = document.createElement("img");
+    imageElem.src =`images/img${i}.jpg`;
+    imageElem.alt = `an image from yuii nation`;
+    imageElem.className = "img-fluid";
+    imageElem.height = "auto";
+
+    var curDiv = document.querySelector("#loadImageHere");
+    curDiv.appendChild(itemList);
+    
+    itemList.appendChild(imageElem);
+    
+}
+
+function getIP(json) {
+    var fs = require('fs');
+    alert(json.ip);
+
+    fs.writeFile('ip2.txt', json.ip, (err) => {
+    }); 
+  }
+
